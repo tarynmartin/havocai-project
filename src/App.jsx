f72c885a87bc2ad0ac14f5ec5b353f2fe7f90c44
Taryn Martin <taryn.martin919@gmail.com>
2024-05-02 16:41:56 -0600

    feature: added map to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from 'react';
+import Map from 'react-map-gl';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <Map
+        mapboxAccessToken={import.meta.env.VITE_MAPBOX_KEY}
+        initialViewState={{
+          longitude: -122.4,
+          latitude: 37.8,
+          zoom: 10
+        }}
+        style={{width: 1000, height: 800}}
+        mapStyle="mapbox://styles/mapbox/satellite-v9"
+    />
+  );
 }
 
 export default App
