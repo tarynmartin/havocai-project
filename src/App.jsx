7eebd53ae4c599adaca13e180a4ae38636add4c3
Taryn Martin <taryn.martin919@gmail.com>
2024-05-03 16:00:23 -0600

    added styling to have map take width of screen

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import Map from 'react-map-gl';
 
 const App = () => {
   return (
+    <div className='app.'>
       <Map
         mapboxAccessToken={import.meta.env.VITE_MAPBOX_KEY}
         initialViewState={{
-          longitude: -122.4,
-          latitude: 37.8,
-          zoom: 10
+          longitude: -91.874,
+          latitude: 42.76,
+          zoom: 12
         }}
-        style={{width: 1000, height: 800}}
+        style={{width: '100vw', height: '100vh' }}
         mapStyle="mapbox://styles/mapbox/satellite-v9"
+        onRender={(e) => e.target.resize()}
     />
+    </div>
   );
 }
 
